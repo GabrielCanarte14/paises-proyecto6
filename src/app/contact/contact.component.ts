8f126dd7e93b0b1927d410da54917e58c928c37f
Meiyin Chang <mmchang@espol.edu.ec>
2023-08-04 01:46:18 -0500

    Graficas actualizadas

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,47 +4,54 @@ import { Data } from '../interfaces/data';
 
 import { LoaderService } from '../providers/loader.service';
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 	public data: Data[] = [];
 	chartOptions = {};
 	chartOptions2 = {};
-	
-	constructor(private dataProvider: LoaderService) {}
-  
+
+	constructor(private dataProvider: LoaderService) { }
+
 	ngOnInit() {
 		this.dataProvider.getRespose().subscribe((response) => {
 			this.data = response as Data[];
 			this.generateCharts();
-		  });
-		}
-	  
+		});
+	}
+
 	generateCharts() {
 		const firstFiveData = this.data.slice(0, 5);
+		const countryarea = this.data.slice(10, 21);
 		this.chartOptions = {
-			title:{
-				text: "Porcentaje de aumento en la población mundial"
-			},
+			animationEnabled: true,
+			theme: "dark2",
+			exportEnabled: true,
+			responsive: true,
+
 			data: [{
-			  type: "column",
-			  dataPoints: firstFiveData.map(countryData => ({ x: countryData.country, y: countryData.worldPercentage })),
+				type: "pie",
+				indexLabel: "{name}: {y}%",
+				dataPoints: firstFiveData.map(countryData => ({ name: countryData.country, y: countryData.worldPercentage })),
 			}]
-		  };
-	  
+		};
+
 		this.chartOptions2 = {
-			title:{
-				text: "Tasa de crecimiento"
-			},
+			animationEnabled: true,
+			theme: "dark2",
+			exportEnabled: true,
+			responsive: true,
 			data: [{
-			  type: "pie",
-			  dataPoints: firstFiveData.map(countryData => ({ name: countryData.country, y: countryData.growthRate })),
+				type: "column",
+			
+				indexLabel: "{} {y}",
+				dataPoints: countryarea.map(countryData => ({ label: countryData.country, y: countryData.landAreaKm })),
 			}]
-		  };
-		}
+		};
 	}
+}
 /*	public data: Data[] = [];
 
 	constructor(private dataProvider: LoaderService) {}
